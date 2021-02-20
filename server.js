45f218975fd1fbe96f8df34529690eee62999f4e
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-20 12:46:37 -0500

    setting up server file

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-const express = require('express');
+var path = require('path');
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
