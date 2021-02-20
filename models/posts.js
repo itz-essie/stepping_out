b68906369016f01ed7f0bec26c61bb5f5247b7d7
Local Al <alexander.s.woolley@gmail.com>
2021-02-20 11:46:34 -0700

    model added to posts.js

---
+++ models/posts.js
@@ -0,0 +1,68 @@
+module.exports = (sequelize, DataTypes) => {
+  const Posts = sequelize.define("Posts", {
+    author_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 80],
+      },
+    },
+    author_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 260],
+      },
+    },
+    activity_category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 260],
+      },
+    },
+    activity_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30],
+      },
+    },
+    activity_description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 500],
+      },
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      validate: {
+        len: [1],
+      },
+    },
+    cost: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      validate: {
+        len: [1, 260],
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 25],
+      },
+    },
+    state_code: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 2],
+      },
+    },
+  });
+  return Posts;
+};
