fe4efa90238d482579f05c8499ca5315dcf4a750
Esiena Ekwofia <eekwofia@gmail.com>
2021-02-23 19:59:16 -0500

    setting up jaws in order to deploy to heroku

--- config/config.js
+++ config/config.js
@@ -1,49 +1,22 @@
-const mysql = require('mysql');
-require('dotenv').config()
-var connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: process.env.DB_PASS,
-  database: 'date_ideas',
-});
-};
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
+require("dotenv").config();
+module.exports=
+{
+  "development": {
+    "username": "root",
+    "password": process.env.DB_PASS,
+    "database": "date_ideas",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "date_ideas",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
   }
-  console.log(`connected as id ${connection.threadId}`);
-});
-module.exports = connection;
-
-
-// require("dotenv").config();
-// module.exports=
-// {
-//   "development": {
-//     "username": "root",
-//     "password": process.env.DB_PASS,
-//     "database": "date_ideas",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": process.env.DB_PASS,
-//     "database": "date_ideas",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": process.env.DB_PASS,
-//     "database": "date_ideas",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
+}
