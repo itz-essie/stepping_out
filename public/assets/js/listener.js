7aa84ff129c66e4ed2bed7956654eebc249b01e5
Esiena Ekwofia <eekwofia@gmail.com>
2021-03-02 13:19:04 -0500

    fixed issue with file upload name change

--- public/assets/js/listener.js
+++ public/assets/js/listener.js
@@ -1,14 +1,25 @@
 const inputState= document.getElementById("inputState");
 let state="";
+const fileUpload = document.getElementById("photoInput");
+const fileName = document.getElementById("currentFileName");
 
-inputState.addEventListener("change", function(event) {
-    state= event.target.value
-    // $.ajax({
-    //     method: "GET",
-    //     url: `/stateselect/${event.target.value}`
-    // })
-    // .then(function(filteredEvent) {
-        console.log("filteredEvent")
-        window.location.href="/stateselect/" + state
-    // })
-})
+// inputState.addEventListener("change", function(event) {
+//     state= event.target.value
+//     // $.ajax({
+//     //     method: "GET",
+//     //     url: `/stateselect/${event.target.value}`
+//     // })
+//     // .then(function(filteredEvent) {
+//         console.log("filteredEvent")
+//         window.location.href="/stateselect/" + state
+//     // })
+// })
+
+fileUpload.addEventListener("change", function(){
+    let newFileUploadArr = fileUpload.files;
+    if (newFileUploadArr.length > 0){
+        fileName.innerText = newFileUploadArr[0].name;
+    } else {
+        fileName.innerText = "Choose File";
+    }
+});
